1c9e1130f69f2d88f84ed1bc96e820dbd7f0594f
yangyilin <yangyilin>
2020-08-25 23:49:18 +0800

    project init

---
+++ mall-coupon/src/test/java/com/young1lin/mall/coupon/MallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.young1lin.mall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
